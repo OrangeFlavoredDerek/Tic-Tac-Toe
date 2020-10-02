680200f0cc8ebc4ed25c0bb1ee7eeb5d23fe421e
Derek <chenjunqi07@icloud.com>
2020-10-02 08:57:35 +0800

    Initial Commit

---
+++ Tic Tac Toe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tic Tac Toe
+//
+//  Created by Derek Chan on 2020/10/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe
+//
+//  Created by Derek Chan on 2020/10/2.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
